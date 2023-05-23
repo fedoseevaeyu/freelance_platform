6df817bb31bbb9cbaf30ba3d1642f44b03a954ec
fedoseevaeyu <eyufedoseeva@gmail.com>
2023-05-23 22:25:46 +0300

    fix заглушка для услуг и убрана галочка

--- apps/web/src/pages/profile/[username]/index.tsx
+++ apps/web/src/pages/profile/[username]/index.tsx
@@ -317,22 +317,6 @@ const ProfilePage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = (
                 <h1 className={clsx('text-3xl font-bold', inter.className)}>
                   {props.name}
                 </h1>
-                {props.verified ? (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-6 w-6 text-blue-500"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M5 13l4 4L19 7"
-                    />
-                  </svg>
-                ) : null}
               </div>
               <span
                 className={clsx(
